70a6d2bd62ead2bd2b8eba276e60773604e6a425
Cataldo Cianciaruso <cianciaruso.cataldo@gmail.com>
2025-02-09 06:27:09 +0100

    clean-up & app theme support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,22 @@
 import "@src/localization";
+import "@styles/core/global.css";
+import "@styles/theme.css";
 import MainProvider from "@src/providers/MainProvider";
-import { appWithTranslation } from "next-i18next";
 import { store } from "@store/index";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
+import { Container } from "mobrix-ui";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <Provider store={store}>
-      <MainProvider>
-        <Component {...pageProps} />
-      </MainProvider>
-    </Provider>
+    <Container data-mbx-page>
+      <Provider store={store}>
+        <MainProvider>
+          <Component {...pageProps} />
+        </MainProvider>
+      </Provider>
+    </Container>
   );
 };
 
-export default appWithTranslation(App);
+export default App;
--- src/localization/index.ts
+++ src/localization/index.ts
@@ -7,29 +7,35 @@ import { store } from "@store/index";
 import { initLocalization } from "@store/slices/localization";
 import { SupportedLanguages } from "./types";
 
+const initializeI18next = async () => {
+  try {
+    await i18next
+      .use(I18NextHttpBackend)
+      .use(LanguageDetector)
+      .use(initReactI18next)
+      .init({
+        fallbackLng: "en",
+        detection: {
+          order: ["querystring", "cookie", "localStorage", "navigator"],
+        },
+        backend: {
+          loadPath: "/locales/{{lng}}/{{ns}}.json",
+        },
+        ns: ["common", "page-titles"],
+        defaultNS: "common",
+        supportedLngs: ["en", "it"],
+      });
+
+    store.dispatch(
+      initLocalization({ lang: i18next.language as SupportedLanguages })
+    );
+  } catch (error) {
+    console.error("i18next initialization failed:", error);
+  }
+};
+
 if (!i18next.isInitialized) {
-  i18next
-    .use(I18NextHttpBackend)
-    .use(LanguageDetector)
-    .use(initReactI18next)
-    .init({
-      fallbackLng: "en",
-      detection: {
-        order: ["querystring", "cookie", "localStorage", "navigator"],
-      },
-      backend: {
-        loadPath: "/locales/{{lng}}/{{ns}}.json",
-      },
-      ns: ["common", "page-titles"],
-      defaultNS: "common",
-    })
-    .then((t) => {
-      store.dispatch(
-        initLocalization({ lang: i18next.language as SupportedLanguages })
-      );
-    })
-    .catch((error) => {
-      console.error("i18next initialization failed:", error);
-    });
+  initializeI18next();
 }
+
 export default i18next;
